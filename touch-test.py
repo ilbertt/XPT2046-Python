0d6d970e30210b058b18a43aa2923f551c8c64dd
Luca Bertelli <luca.berte98@hotmail.it>
2020-10-01 21:48:55 +0200

    Create touch-test.py

---
+++ touch-test.py
@@ -0,0 +1,23 @@
+from xpt2046 import Touch
+from gpiozero import Button, DigitalOutputDevice
+import board
+import busio
+from time import sleep
+
+# touch callback
+def touchscreen_press(x, y):
+    print(x,y)
+
+cs = DigitalOutputDevice(17)
+clk = board.SCLK_1		# same as writing 21
+mosi = board.MOSI_1	# same as writing 20
+miso = board.MISO_1	# same as writing 19
+irq = Button(26)
+
+spi = busio.SPI(clk, mosi, miso)	# auxiliary SPI
+
+xpt = Touch(spi, cs=cs, int_pin=irq, int_handler=touchscreen_press)
+
+while True:
+    #print(xpt.get_touch()) # to get the (x, y) coords when you desire
+    sleep(.01)
